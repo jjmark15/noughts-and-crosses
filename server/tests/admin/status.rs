6948c62c733bc711bffe0e714af750a38d5250dc
Josh Jones <ohblonddev@gmail.com>
2021-02-15 19:07:08 +0000

    Run functional tests against server started from docker-compose

--- server/tests/admin/status.rs
+++ server/tests/admin/status.rs
@@ -3,12 +3,8 @@ use spectral::prelude::*;
 use functional_testing::http::{Client, Method, StatusCode};
 use functional_testing::response::AppStatusResponse;
 
-use crate::common::server_handle;
-
 #[tokio::test]
 async fn returns_status() {
-    let _server_handle = server_handle().await;
-
     let response = Client::new()
         .request(Method::GET, "http://localhost:3030/admin/status")
         .send()
--- server/tests/game/create_room.rs
+++ server/tests/game/create_room.rs
@@ -3,12 +3,8 @@ use spectral::prelude::*;
 use functional_testing::http::{Client, Method, StatusCode};
 use functional_testing::response::CreatedRoomResponse;
 
-use crate::common::server_handle;
-
 #[tokio::test]
 async fn creates_room_with_room_id() {
-    let _server_handle = server_handle().await;
-
     let response = Client::new()
         .request(Method::POST, "http://localhost:3030/game/rooms")
         .send()
