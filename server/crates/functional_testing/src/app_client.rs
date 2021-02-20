e38413dfc4fe197cd3191b290419eb8aaffd41d4
Josh Jones <ohblonddev@gmail.com>
2021-02-20 14:09:58 +0000

    Refactor app client

--- server/crates/functional_testing/src/app_client.rs
+++ server/crates/functional_testing/src/app_client.rs
@@ -13,25 +13,30 @@ impl AppClient {
         }
     }
 
+    fn base_request_url(&self, path: &str) -> String {
+        format!("http://{}{}", self.server_address.as_str(), path)
+    }
+
+    async fn build_and_send_request(
+        &self,
+        request_builder: reqwest::RequestBuilder,
+    ) -> reqwest::Response {
+        request_builder.send().await.unwrap()
+    }
+
     pub async fn status(&self) -> reqwest::Response {
-        self.http_client
-            .request(
-                Method::GET,
-                format!("http://{}/admin/status", self.server_address.as_str()).as_str(),
-            )
-            .send()
-            .await
-            .unwrap()
+        let request = self
+            .http_client
+            .request(Method::GET, self.base_request_url("/admin/status").as_str());
+
+        self.build_and_send_request(request).await
     }
 
     pub async fn create_room(&self) -> reqwest::Response {
-        self.http_client
-            .request(
-                Method::POST,
-                format!("http://{}/game/rooms", self.server_address.as_str()).as_str(),
-            )
-            .send()
-            .await
-            .unwrap()
+        let request = self
+            .http_client
+            .request(Method::POST, self.base_request_url("/game/rooms").as_str());
+
+        self.build_and_send_request(request).await
     }
 }
