41f0cbae98ee1c591bffb73b46aa154685fbdf11
Josh Jones <ohblonddev@gmail.com>
2021-03-10 17:34:46 +0000

    refactor FT feature names

--- server/tests/api/game/get_user_name.rs
+++ server/tests/api/game/get_user_name.rs
@@ -7,44 +7,6 @@ use functional_testing::response::{RegisteredUserResponse, SimpleErrorResponse};
 use crate::helpers::app_client;
 
 #[tokio::test]
-async fn registers_user_with_name() {
-    let app_client = app_client();
-
-    let register_response = app_client.register_user("Name").await;
-
-    assert_that(&register_response.status()).is_equal_to(StatusCode::CREATED);
-
-    let id = register_response
-        .json::<RegisteredUserResponse>()
-        .await
-        .unwrap()
-        .user_id()
-        .unwrap();
-    let name = app_client.user_name(id).await.text().await.unwrap();
-
-    assert_that(&name).is_equal_to(&"Name".to_string());
-}
-
-#[tokio::test]
-async fn registers_user_with_name_including_spaces() {
-    let app_client = app_client();
-
-    let register_response = app_client.register_user("First Last").await;
-
-    assert_that(&register_response.status()).is_equal_to(StatusCode::CREATED);
-
-    let id = register_response
-        .json::<RegisteredUserResponse>()
-        .await
-        .unwrap()
-        .user_id()
-        .unwrap();
-    let name = app_client.user_name(id).await.text().await.unwrap();
-
-    assert_that(&name).is_equal_to(&"First Last".to_string());
-}
-
-#[tokio::test]
 async fn returns_name_given_user_id() {
     let app_client = app_client();
 
--- server/tests/api/game/mod.rs
+++ server/tests/api/game/mod.rs
@@ -1,5 +1,6 @@
-mod create_new_room;
+mod create_room;
+mod get_user_name;
 mod join_room;
-mod leave_room_if_disconnects;
+mod leave_room;
+mod register_user;
 mod start_new_game;
-mod users;
